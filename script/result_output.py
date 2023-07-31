5743b0cdb12ba9ed5e8dc244029902f0504180a6
pradeepgs50 <122854550+pradeepgs50@users.noreply.github.com>
2023-07-31 13:14:24 +0530

    Update result_output.py

--- script/result_output.py
+++ script/result_output.py
@@ -1,22 +1,25 @@
 import json,os,time,logging
 
 class ResultOutput:
-    counter=0
-    output=None
-    testcases=[]
-    summary= {
-        "totalTests": 0,
-        "Passed": 0,
-        "Failed": 0,
-        "Errored": 0,
-        "eval":1
-    }
-    eval_message={}
 
+    #index =0
     def __init__(self,args,class_object):
         # logging.info("step 1")
         # print("entered init block")
-        testcase_list=[]
+        self.index=-1
+        self.output=None
+        self.testcases=[]
+        self.testcase_method=""
+        self.method_list=None
+        #current_testcase_method=""
+        self.summary= {
+            "totalTests": 0,
+            "Passed": 0,
+            "Failed": 0,
+            "Errored": 0,
+            "eval":1
+        }
+        self.eval_message={}
         time.sleep(1)
         try:
             #logging.info("step 2")
@@ -32,12 +35,7 @@ class ResultOutput:
             #logging.info("step 4")
         except Exception as e:
             logging.info(str(e))   
-
-        # try:
-        #     json.loads(args)
-        # except Exception as e:
-        #     logging.info(str(e))
-        #     logging.info("Malformed json input argumnets")            
+     
         
         try: 
             if "token" in json.loads(args).keys():
@@ -53,12 +51,14 @@ class ResultOutput:
             logging.info(str(e))
             logging.info("Malformed json input argumnets") 
 
-        method_list = [attribute for attribute in dir(class_object) if callable(getattr(class_object, attribute)) and attribute.startswith('testcase') is True]
-        index=0
-        #print(method_list)
-        #logging.info("step 9")
-        for testcase_method in method_list:
-            template={"index":0,
+        self.method_list = [attribute for attribute in dir(class_object) if callable(getattr(class_object, attribute)) and attribute.startswith('testcase') is True]
+
+        #self.testcases=testcase_list
+        #logging.info("step 11")
+
+    def update_pre_result(self,description="",expected=""):
+
+        template={"index":0,
             "testCase": "",
             "expected": "",
             "actual": "",
@@ -66,48 +66,49 @@ class ResultOutput:
             "comments": "",
             "ref": ""
             }
-            template["index"]=index
-            template["testCase"]="{{"+str(testcase_method)+"_description"+"}}"
-            template["expected"]="{{"+str(testcase_method)+"_expected"+"}}"
-            template["actual"]="{{"+str(testcase_method)+"_actual"+"}}"
-            template["status"]=0
-            template["comments"]="{{"+str(testcase_method)+"_comments"+"}}"
-            template["ref"]="{{"+str(testcase_method)+"_ref"+"}}"
-            testcase_list.append(template)
-            index+=1
-        self.testcases=testcase_list
-        #logging.info("step 11")
+        self.index+=1
+    #self.testcase_method != self.method_list[ self.index+=1]:
+        self.testcase_method=self.method_list[ self.index]
+        template["index"]=self.index
+        template["testCase"]="{{"+str(self.method_list[self.index])+"_description"+"}}"
+        template["expected"]="{{"+str(self.method_list[self.index])+"_expected"+"}}"
+        template["actual"]="{{"+str(self.method_list[self.index])+"_actual"+"}}"
+        template["status"]=0
+        template["comments"]="{{"+str(self.method_list[self.index])+"_comments"+"}}"
+        template["ref"]="{{"+str(self.method_list[self.index])+"_ref"+"}}"
+        self.testcases.append(template)
+        self.testcases[self.index]["testCase"]=description
+        self.testcases[self.index]["expected"]=expected
+        self.summary["totalTests"]+=1
+            
 
-    def update_pre_result(self,description="",expected=""):
-        self.testcases[self.counter]["testCase"]=description
-        self.testcases[self.counter]["expected"]=expected
+    def update_result(self,result,expected=None,actual=None,comment=None,ref=None):
+        if actual !=None:
+            self.testcases[self.index]["expected"]=expected 
+        if comment !=None:
+            self.testcases[self.index]["comments"]=comment
+        if actual !=None:
+            self.testcases[self.index]["actual"]=actual 
+        if ref !=None:
+            self.testcases[self.index]["ref"]=ref        
+        self.testcases[self.index]["status"]=result
 
-    def update_result(self,result,expected=" ",actual=" ",comment="",ref=""):
-        if actual != "":
-            self.testcases[self.counter]["expected"]=expected 
-        if comment != "":
-            self.testcases[self.counter]["comments"]=comment
-        if actual != "":
-            self.testcases[self.counter]["actual"]=actual 
-        if ref != "":
-            self.testcases[self.counter]["ref"]=ref        
-        
-        self.testcases[self.counter]["status"]=result
         if result == 1:
             self.summary["Passed"]+=1
+            #self.summary["totalTests"]+=1
         elif result == 0:
             self.summary["Failed"]+=1
+            #self.summary["totalTests"]+=1
         elif result == -1:
             self.summary["Errored"]+=1
             self.summary["eval"]=0
-
-        self.summary["totalTests"]+=1
-        self.counter+=1 
+            #self.summary["totalTests"]+=1
+        #print(self.testcases)
         return 
 
     def result_final(self):
         self.output["testCases"]=self.testcases
-        self.output["summary"]["totalTests"]=self.summary["totalTests"]
+        self.output["summary"]["totalTests"]=self.summary["totalTests"]+self.summary["Errored"]
         self.output["summary"]["Passed"]=self.summary["Passed"]
         self.output["summary"]["Failed"]=self.summary["Failed"]
         self.output["summary"]["Errored"]=self.summary["Errored"]

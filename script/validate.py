3d2dabc556d7fd320896764018aea2aec1626873
Greeta Pearl <117262728+GreetaPearlNuvepro@users.noreply.github.com>
2023-08-04 18:59:07 +0530

    Update validate.py

--- script/validate.py
+++ script/validate.py
@@ -14,67 +14,67 @@ from kubernetes import client
 
 class Activity():
 
-    def testcase_check_GKE_Cluster_name(self,test_object,credentials,project_id):
-        testcase_description="Check GKE Cluster name"
-        expected_result='gke-cluster-1'
-        try:
-            is_present = False
-            actual = 'GKE Cluster name is not '+ expected_result
-            try:
-                service = discovery.build('container', 'v1', credentials=credentials)
-                request = service.projects().zones().clusters().list(projectId=project_id, zone='-')
-                response = request.execute()  
-                if 'clusters' in response:
-                    for cluster in response['clusters']:
-                        if cluster['name'] == expected_result:
-                            is_present = True
-                            actual=expected_result
-                            break
-                        else:
-                            actual = cluster['name']
-                            pass
-            except Exception as e:
-                is_present = False
-            test_object.update_pre_result(testcase_description,expected_result)
-            if is_present==True:
-                test_object.update_result(1,expected_result,actual,"No Comment"," Congrats! You have done it right!") 
-            else:
-                test_object.update_result(0,expected_result,actual,"Check GKE Cluster","https://cloud.google.com/kubernetes-engine/docs/concepts/autopilot-overview?_ga=2.2930998.-76994253.1691030875")   
+    # def testcase_check_GKE_Cluster_name(self,test_object,credentials,project_id):
+    #     testcase_description="Check GKE Cluster name"
+    #     expected_result='gke-cluster-1'
+    #     try:
+    #         is_present = False
+    #         actual = 'GKE Cluster name is not '+ expected_result
+    #         try:
+    #             service = discovery.build('container', 'v1', credentials=credentials)
+    #             request = service.projects().zones().clusters().list(projectId=project_id, zone='-')
+    #             response = request.execute()  
+    #             if 'clusters' in response:
+    #                 for cluster in response['clusters']:
+    #                     if cluster['name'] == expected_result:
+    #                         is_present = True
+    #                         actual=expected_result
+    #                         break
+    #                     else:
+    #                         actual = cluster['name']
+    #                         pass
+    #         except Exception as e:
+    #             is_present = False
+    #         test_object.update_pre_result(testcase_description,expected_result)
+    #         if is_present==True:
+    #             test_object.update_result(1,expected_result,actual,"No Comment"," Congrats! You have done it right!") 
+    #         else:
+    #             test_object.update_result(0,expected_result,actual,"Check GKE Cluster","https://cloud.google.com/kubernetes-engine/docs/concepts/autopilot-overview?_ga=2.2930998.-76994253.1691030875")   
 
-        except Exception as e:    
-            test_object.update_result(-1,expected_result,"Internal Server error","Please check with Admin","")
-            test_object.eval_message["testcase_check_GKE_Cluster_name"]=str(e)                
+    #     except Exception as e:    
+    #         test_object.update_result(-1,expected_result,"Internal Server error","Please check with Admin","")
+    #         test_object.eval_message["testcase_check_GKE_Cluster_name"]=str(e)                
 
-    def testcase_check_Node_numbers(self,test_object,credentials,project_id):
-        testcase_description="Check GKE Cluster Nodes Count"
-        expected_result=1
-        try:
-            is_present = False
-            actual = 'GKE Cluster nodes is not '+ str(expected_result)
-            try:
-                service = discovery.build('container', 'v1', credentials=credentials)
-                request = service.projects().zones().clusters().list(projectId=project_id, zone='-')
-                response = request.execute()  
-                if 'clusters' in response:
-                    for cluster in response['clusters']:
-                        if cluster['currentNodeCount'] == expected_result:
-                            is_present = True
-                            actual=expected_result
-                            break
-                        else:
-                            actual=cluster['currentNodeCount']
-                            pass
-            except Exception as e:
-                is_present = False
-            test_object.update_pre_result(testcase_description,expected_result)
-            if is_present==True:
-                test_object.update_result(1,expected_result,actual,"No Comment"," Congrats! You have done it right!") 
-            else:
-                test_object.update_result(0,expected_result,actual,"Check Node","https://cloud.google.com/kubernetes-engine/docs")   
+    # def testcase_check_Node_numbers(self,test_object,credentials,project_id):
+    #     testcase_description="Check GKE Cluster Nodes Count"
+    #     expected_result=1
+    #     try:
+    #         is_present = False
+    #         actual = 'GKE Cluster nodes is not '+ str(expected_result)
+    #         try:
+    #             service = discovery.build('container', 'v1', credentials=credentials)
+    #             request = service.projects().zones().clusters().list(projectId=project_id, zone='-')
+    #             response = request.execute()  
+    #             if 'clusters' in response:
+    #                 for cluster in response['clusters']:
+    #                     if cluster['currentNodeCount'] == expected_result:
+    #                         is_present = True
+    #                         actual=expected_result
+    #                         break
+    #                     else:
+    #                         actual=cluster['currentNodeCount']
+    #                         pass
+    #         except Exception as e:
+    #             is_present = False
+    #         test_object.update_pre_result(testcase_description,expected_result)
+    #         if is_present==True:
+    #             test_object.update_result(1,expected_result,actual,"No Comment"," Congrats! You have done it right!") 
+    #         else:
+    #             test_object.update_result(0,expected_result,actual,"Check Node","https://cloud.google.com/kubernetes-engine/docs")   
 
-        except Exception as e:    
-            test_object.update_result(-1,expected_result,"Internal Server error","Please check with Admin","")
-            test_object.eval_message["testcase_check_Node_numbers"]=str(e)                
+    #     except Exception as e:    
+    #         test_object.update_result(-1,expected_result,"Internal Server error","Please check with Admin","")
+    #         test_object.eval_message["testcase_check_Node_numbers"]=str(e)                
 
     def testcase_check_Workload_name(self,test_object,credentials,project_id):
         print("Token is ")
@@ -88,7 +88,7 @@ class Activity():
             try:
                 
                 # cluster_id="gke-cluster-1"
-                cluster_id="gke-nginx-cluster”
+                # cluster_id="gke-nginx-cluster"
                 zone = "us-central1-c"
                 service = discovery.build('container', 'v1', credentials=credentials)
                 request = service.projects().zones().clusters().list(projectId=project_id, zone='-')
@@ -138,9 +138,11 @@ def start_tests(credentials, project_id, args):
     
     test_object=ResultOutput(args,Activity)
     challenge_test=Activity()
-    #challenge_test.testcase_check_GKE_Cluster_name(test_object,credentials,project_id)
-    #challenge_test.testcase_check_Node_numbers(test_object,credentials,project_id)
-    challenge_test.testcase_check_Workload_name(test_object,credentials,project_id)
+    print("Token is ")
+    print(credentials.token)
+    # challenge_test.testcase_check_GKE_Cluster_name(test_object,credentials,project_id)
+    # challenge_test.testcase_check_Node_numbers(test_object,credentials,project_id)
+    # challenge_test.testcase_check_Workload_name(test_object,credentials,project_id)
 
     json.dumps(test_object.result_final(),indent=4)
     return test_object.result_final()
